b239e51e4cf15e3f7caa28b2f3ec76c4f81ddb49
Ежевичка <nickolay.simonov@outlook.com>
2016-01-24 16:03:33 +0400

    improves

--- CachedImageLoader/CachedImageLoader.cs
+++ CachedImageLoader/CachedImageLoader.cs
@@ -22,6 +22,9 @@ using System.IO;
 using Android.Widget;
 using Android.Util;
 using System.Security.Cryptography;
+using Java.Util;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace co.littlebyte.Utils
 {
@@ -36,11 +39,14 @@ namespace co.littlebyte.Utils
 
 		private readonly string _url;
 
+		private static Dictionary<string,Bitmap> _staticCache = new Dictionary<string,Bitmap> ();
+
 		public CachedImageLoader (string url)
 		{
 			_url = url;
 		}
 
+		/*
 		private static string GetMD5 (string s)
 		{
 			MD5 md5 = MD5.Create ();
@@ -53,14 +59,42 @@ namespace co.littlebyte.Utils
 
 			return res;
 		}
+		*/
 
-		public async void Load (BitmapFactory.Options o = null)
+		public static string GetMD5 (string str)
+		{
+			try {
+				var s = new Java.Lang.String (str);
+
+				// Create MD5 Hash
+				var digest = Java.Security.MessageDigest.GetInstance ("MD5");
+				digest.Update (s.GetBytes ());
+				byte[] messageDigest = digest.Digest ();
+
+				// Create Hex String
+				Java.Lang.StringBuffer hexString = new Java.Lang.StringBuffer ();
+				for (int i = 0; i < messageDigest.Length; i++) {
+					String h = Java.Lang.Integer.ToHexString (0xFF & messageDigest [i]);
+					while (h.Length < 2)
+						h = "0" + h;
+					hexString.Append (h);
+				}
+				return hexString.ToString ();
+
+			} catch (Java.Security.NoSuchAlgorithmException ex) {
+				Log.Error (TAG, ex.StackTrace);
+			}
+			return "";
+		}
+
+		public void Load (BitmapFactory.Options o = null, int scaleWidth = -1, int scaleHeight = -1)
 		{
 			Bitmap bmp = null;
 			var uri = new Uri (_url);
 			var client = new WebClient ();
 
 			string url_hash = GetMD5 (_url);
+
 			var path = Application.Context.CacheDir.AbsolutePath + "/" + url_hash;
 			var f_info = new FileInfo (path);
 
@@ -68,30 +102,71 @@ namespace co.littlebyte.Utils
 				o = new BitmapFactory.Options (){ InPurgeable = true };
 			}
 
-			if (f_info.Exists) {
-				var ba = File.ReadAllBytes (path);
-				bmp = await BitmapFactory.DecodeByteArrayAsync (ba, 0, ba.Length, o);
+			if (_staticCache.ContainsKey (url_hash)) {
 				if (OnLoad != null) {
-					OnLoad (this, bmp);
+					OnLoad (this, _staticCache [url_hash]);
 				}
+				return;
+			}
+
+			if (f_info.Exists) {
+				
+				Task task = new Task (() => {
+					
+					var ba = File.ReadAllBytes (path);
+
+					bmp = BitmapFactory.DecodeByteArray (ba, 0, ba.Length, o);
+
+					_staticCache [url_hash] = bmp;
+
+					if (OnLoad != null) {
+						OnLoad (this, bmp);
+					}
+
+				});
+				task.Start ();
+
 			} else {
 				client.DownloadDataCompleted += async (sender, e) => {
 					if (!e.Cancelled && e.Error == null) {
 						if (e.Result != null && e.Result.Length > 0) {
 							bmp = await BitmapFactory.DecodeByteArrayAsync (e.Result, 0, e.Result.Length, o);
-							f_info.Directory.Create ();
-							File.WriteAllBytes (path, e.Result);
-							if (OnLoad != null) {
-								OnLoad (this, bmp);
+
+							if (scaleWidth > 0 && scaleHeight > 0) {
+								bmp = Bitmap.CreateScaledBitmap (bmp, scaleWidth, scaleHeight, true);
 							}
+								
+							Task task = new Task (() => {
+
+								f_info.Directory.Create ();
+
+								var fileStream = new FileStream (path, 
+									                 FileMode.OpenOrCreate, 
+									                 FileAccess.ReadWrite, 
+									                 FileShare.None);
+
+								var compress = bmp.Compress (Bitmap.CompressFormat.Png, 100, fileStream);
+
+								fileStream.Close ();
+
+								_staticCache [url_hash] = bmp;
+							
+								if (OnLoad != null) {
+									OnLoad (this, bmp);
+								}
+
+							});
+							task.Start ();
+
 						}
 					}
 				};
 				try {
 					client.DownloadDataAsync (uri);
 				} catch (WebException ex) {
-					Log.Error (TAG, ex.Message);
+					Log.Error (TAG, ex.StackTrace);
 					var eh = OnException;
+
 					if (eh != null) {
 						OnException (this, ex);
 					}
@@ -99,7 +174,8 @@ namespace co.littlebyte.Utils
 			}
 		}
 
-		public static void LoadImageFromUrl (ImageView iv, string url, BitmapFactory.Options options)
+
+		public static void LoadImageFromUrl (ImageView iv, string url, BitmapFactory.Options options, int scaleWidth = -1, int scaleHeight = -1)
 		{
 			var l = new CachedImageLoader (url);
 
@@ -108,7 +184,8 @@ namespace co.littlebyte.Utils
 					iv.SetImageBitmap (e);
 				}
 			};
-			l.Load (options);
+
+			l.Load (options, scaleWidth, scaleHeight);
 		}
 	}
 }
