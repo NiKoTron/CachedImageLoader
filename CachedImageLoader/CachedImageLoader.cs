0422822cc03f44d1378f20e655dcaea74b00aa14
Ежевичка <nickolay.simonov@outlook.com>
2016-04-06 12:23:38 +0400

    bug fixes and go async

--- CachedImageLoader/CachedImageLoader.cs
+++ CachedImageLoader/CachedImageLoader.cs
@@ -11,6 +11,12 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+CONTRIBUTORS:
+- Nickolay Simonov <nikotron@rocketmail.com>
+- Eugeny Polunin
+
 */
 
 using System;
@@ -21,6 +27,8 @@ using System.IO;
 using Android.Widget;
 using Android.Util;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace co.littlebyte.Utils
@@ -31,21 +39,16 @@ namespace co.littlebyte.Utils
 		public int Quality = 100;
 	}
 
-	public class Bob
-	{
-		public Bitmap Bitmap { get; set; }
-		 
-	}
-
 	public class CachedImageLoader
 	{
 		private static readonly string TAG = "CachedImageLoader";
 
-		public event EventHandler<Bitmap> OnLoad;
-		public event EventHandler<Exception> OnException;
-
 		public BitmapFactory.Options BitmapOptions { get; set; }
 
+		private  Queue<Tuple<string,Bitmap, FileInfo, BitmapCompressOptions>> _writeQueue = new Queue<Tuple<string, Bitmap, FileInfo, BitmapCompressOptions>> ();
+		private static SemaphoreSlim _loadQueueSemaphore = new SemaphoreSlim (1, 1);
+
+		private static List<ImageView> _loadedImageViews = new List<ImageView> ();
 		private readonly string _url;
 
 		private static readonly Dictionary<string, Bitmap> _staticRAMCache = new Dictionary<string, Bitmap> ();
@@ -53,6 +56,7 @@ namespace co.littlebyte.Utils
 		public static void ClearRAMCache ()
 		{
 			_staticRAMCache.Clear ();
+			_loadedImageViews.Clear ();
 		}
 
 		public CachedImageLoader (string url)
@@ -80,12 +84,14 @@ namespace co.littlebyte.Utils
 				return hexString.ToString ();
 
 			} catch (Java.Security.NoSuchAlgorithmException ex) {
-				Log.Error (TAG, ex.StackTrace);
+				Log.Debug (TAG, ex.StackTrace);
 			}
 			return "";
 		}
 
-		public static int CalculateInSampleSize (BitmapFactory.Options options, int reqWidth, int reqHeight)
+
+		public static int CalculateInSampleSize (
+			BitmapFactory.Options options, int reqWidth, int reqHeight)
 		{
 			// Raw height and width of image
 			int height = options.OutHeight;
@@ -108,117 +114,105 @@ namespace co.littlebyte.Utils
 			return inSampleSize;
 		}
 
-		public void Load (BitmapFactory.Options o = null, int scaleWidth = -1, int scaleHeight = -1, BitmapCompressOptions compressOptions = null)
+		public async Task<Bitmap> LoadInternal (BitmapFactory.Options o = null, int scaleWidth = -1,
+		                                        int scaleHeight = -1, BitmapCompressOptions compressOptions = null)
 		{
+			if (String.IsNullOrEmpty (_url)) {
+				return null;
+			}
+
 			Bitmap bmp = null;
 
 			if (compressOptions == null) {
 				compressOptions = new BitmapCompressOptions ();
 			}
 
-			var uri = new Uri (_url);
-			var client = new WebClient ();
-
-			var urlHash = GetMD5 (new Java.Lang.String (string.Format ("{0}{1}{2}", _url, scaleWidth, scaleHeight)));
-
-			var path = Application.Context.CacheDir.AbsolutePath + "/" + urlHash;
-			var fileInfo = new FileInfo (path);
-
-			if (o == null) {
-				o = new BitmapFactory.Options () { InPurgeable = true };
-			}
-
-			if (_staticRAMCache.ContainsKey (urlHash)) {
-				if (OnLoad != null) {
-					OnLoad (this, _staticRAMCache [urlHash]);
-				}
-				return;
-			}
-
-			if (fileInfo.Exists) {
-
-				var task = new Task (() => {
-					var ba = File.ReadAllBytes (path);
-					bmp = BitmapFactory.DecodeByteArray (ba, 0, ba.Length, o);
-
-					_staticRAMCache [urlHash] = bmp;
-
-					if (OnLoad != null) {
-						OnLoad.Invoke (this, bmp);
-					}
-				});
-				task.Start ();
-				return;
-			}
-
-			client.DownloadDataCompleted += async (sender, e) => {
-				if (e.Cancelled || e.Error != null)
-					return;
-
-				if (e.Result == null || e.Result.Length <= 0)
-					return;
-
-				if (scaleWidth > 0 && scaleHeight > 0) {
-					o.InSampleSize = CalculateInSampleSize (o, scaleWidth, scaleHeight);
-				}
-
-				bmp = await BitmapFactory.DecodeByteArrayAsync (e.Result, 0, e.Result.Length, o);
-
-				if (scaleWidth > 0 && scaleHeight > 0) {
-					//bmp = Bitmap.CreateScaledBitmap(bmp, scaleWidth, scaleHeight, true);
-				}
-
-				var task = new Task (() => {
-					if (fileInfo.Directory != null) {
-						fileInfo.Directory.Create ();
-					}
-
-					var fileStream = new FileStream (path,
-						                 FileMode.OpenOrCreate,
-						                 FileAccess.ReadWrite,
-						                 FileShare.None);
-
-					bmp.Compress (compressOptions.Format, compressOptions.Quality, fileStream);
-
-					fileStream.Close ();
-
-					_staticRAMCache [urlHash] = bmp;
-
-					if (OnLoad != null) {
-						OnLoad (this, bmp);
-					}
-
-				});
-				task.Start ();
-			};
-
 			try {
-				client.DownloadDataAsync (uri);
-			} catch (WebException ex) {
-				Log.Error (TAG, ex.StackTrace);
-				var eh = OnException;
+				var uri = new Uri (_url);
+				var client = new WebClient ();
+				var stringme = String.Format ("{0}{1}{2}", _url, scaleWidth, scaleHeight);
+				var urlHash = GetMD5 (new Java.Lang.String (stringme));
 
-				if (eh != null) {
-					if (OnException != null) {
-						OnException (this, ex);
+				var path = Application.Context.CacheDir.AbsolutePath + "/" + urlHash;
+				var fileInfo = new FileInfo (path);
+
+				if (o == null) {
+					o = new BitmapFactory.Options () { InPurgeable = true };
+				}
+
+				if (_staticRAMCache.ContainsKey (urlHash)) {
+					return _staticRAMCache [urlHash];
+				}
+
+				if (fileInfo.Exists) {
+					using (var file = File.Open (path, FileMode.Open, FileAccess.Read)) {
+						byte[] buff = new byte[file.Length];
+						await file.ReadAsync (buff, 0, (int)file.Length);
+						bmp = await BitmapFactory.DecodeByteArrayAsync (buff, 0, buff.Length, o);
+						_staticRAMCache [urlHash] = bmp;
+
+						return bmp;
+					}
+				} else {
+					try {
+						Log.Debug ("Loading " + uri.ToString ());
+						var response = await client.DownloadDataTaskAsync (uri);
+
+						if (response == null || response.Length <= 0)
+							return null;
+
+						if (scaleWidth > 0 && scaleHeight > 0) {
+							o.InSampleSize = CalculateInSampleSize (o, scaleWidth, scaleHeight);
+						}
+
+						bmp = await BitmapFactory.DecodeByteArrayAsync (response, 0, response.Length, o);
+						if (fileInfo.Directory != null) {
+							fileInfo.Directory.Create ();
+						}
+
+						var fileStream = new FileStream (path,
+							                 FileMode.OpenOrCreate,
+							                 FileAccess.ReadWrite,
+							                 FileShare.ReadWrite);
+
+						await bmp.CompressAsync (compressOptions.Format, compressOptions.Quality, fileStream);
+
+						fileStream.Close ();
+
+						_staticRAMCache [urlHash] = bmp;
+
+						return bmp;
+
+					} catch (WebException ex) {
+						Log.Debug (TAG, ex.StackTrace);
+						return null;
 					}
 				}
+			} catch (System.UriFormatException ex) {
+				Log.Debug (TAG, ex.StackTrace);
+				return null;
 			}
 		}
 
-		public static void LoadImageFromUrl (ImageView iv, string url, BitmapFactory.Options options, int scaleWidth = -1, int scaleHeight = -1, BitmapCompressOptions compressOptions = null)
+
+		public async static void LoadImageFromUrl (ImageView iv, string url, BitmapFactory.Options options = null, int scaleWidth = -1, int scaleHeight = -1, BitmapCompressOptions compressOptions = null)
 		{
+			await _loadQueueSemaphore.WaitAsync ();
+
+			if (_loadedImageViews.Contains (iv))
+				_loadedImageViews.Remove (iv);
 			var l = new CachedImageLoader (url);
 
-			l.OnLoad += (sender, e) => {
-				if (e != null) {
-					iv.SetImageBitmap (e);
-				}
-			};
+			var bitmap = await l.LoadInternal (options, scaleWidth, scaleHeight, compressOptions);
+			_loadQueueSemaphore.Release ();
+			if (bitmap != null && !_loadedImageViews.Contains (iv)) {
 
-			l.Load (options, scaleWidth, scaleHeight, compressOptions);
+				iv.SetImageBitmap (bitmap);
+				_loadedImageViews.Add (iv);
+			}
+
+
 		}
-	
 	}
 }
 
